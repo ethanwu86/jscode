4a7c88080b8bcf6973d16b67fa6b64ff59ad5d9e
Ethan <wuyusheng86@hotmail.com>
2018-03-08 23:29:14 +0400

    fixbeam

--- tets.js
+++ tets.js
@@ -1,2 +1,11 @@
-var num=1;
-// this is a comment;
+var nbelems = ixforten_model.nb_elems();
+for (var i = 0; i < nbelems; i++) {
+	var obj = ixforten_model.element(i);
+	if (obj.get_property("fem_type") == "beam") {
+		var nodeA = obj.node(0);
+		var nodeB = obj.node(1);
+		nodeA.set_property("restraint", 63);
+		nodeB.set_property("restraint", 63);
+		
+	}
+}
